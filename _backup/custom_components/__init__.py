d80b5a7494ddf12fff5d97ebf99bc058e17ca51f
Michal Kapusnik <michal.kapusnik@gmail.com>
2026-02-21 22:19:13 +0100

    Cleanup original implementation

--- _backup/custom_components/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Namespace package for Home Assistant custom components used in tests.
-
-This file allows Python to treat `custom_components` as a package
-when running unit tests outside of Home Assistant.
-"""
--- _backup/test/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test package for BVK integration."""
